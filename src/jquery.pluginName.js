382e1835a2e852ce6d04c851dab92af7ecbc385f
Cesar Kastli <cesarkastli@gmail.com>
2015-03-12 15:55:17 -0300

    first commit

---
+++ src/jquery.pluginName.js
@@ -0,0 +1,34 @@
+;(function($, window, document, undefined) {
+
+  'use strict';
+
+  var pluginName = 'pluginName';
+  
+  var defaults = {};
+
+  var Plugin = function(element, options) {
+    this._element = element;
+    this._options = $.extend({}, defaults, options, $(element).data());
+  };
+
+  Plugin.prototype._init = function() {
+    console.log('Hello world');
+  };
+
+  var pluginSetup = function(options) {
+    $.extend(defaults, options);
+  };
+
+  var pluginHandler = function(options) {
+    return this.each(function() {
+      if (!$(this).data('plugin_' + pluginName)) {
+        $(this).data('plugin_' + pluginName, new Plugin(this, options));
+      }
+    });
+  };
+
+  $[pluginName] = pluginSetup;
+  $.fn[pluginName] = pluginHandler;
+  $.fn[pluginName].defaults = defaults;
+
+}(jQuery, window, document));
